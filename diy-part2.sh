a26db55472a4ab522979d4faa6c017a22d43364b
vanminh0311 <85997835+vanminh0311@users.noreply.github.com>
2022-04-20 09:40:11 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,3 +16,13 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+	sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
+	echo "DISTRIB_REVISION='Router'" >> /etc/openwrt_release
+	sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
+	echo "DISTRIB_DESCRIPTION='Os'" >> /etc/openwrt_release
+
+	sed -i '/REDIRECT --to-ports 53/d' /etc/firewall.user
+	echo 'iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
+	echo 'iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
+	echo '[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
+	echo '[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user
